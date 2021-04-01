a3511b7e99f827ac89df17bef2d0dc3339cdb90a
c-shiraga <cs310_rb@icloud.com>
2021-04-01 12:29:45 +0900

    gitignoeを変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.web_console.whitelisted_ips = [ "172.16.0.0/12" ]
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
 end
