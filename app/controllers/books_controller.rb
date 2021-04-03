dfb1ff818612cffd1baaff0e2a9c5df55a55a917
c-shiraga <cs310_rb@icloud.com>
2021-04-04 01:44:21 +0900

    パスワード再設定機能を追加、投稿詳細ページを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   # アクションのフィルターをかける
-  before_action :authorize
+  # before_action :authorize
+  before_action :authenticate_user!
 
   def create
     @book = Book.new(book_params)
@@ -8,8 +9,8 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to books_path
     else
-      @books = Book.all
-      render 'index'
+      @books = Book.all.order(id: "DESC")
+      render action: :new
     end
   end
 
@@ -19,8 +20,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.new
-    @books = Book.all.order(id: "DESC")
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -31,16 +31,16 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy!
-    redirect_to action: :show
+    redirect_to action: :new
   end
 
 
   # ユーザーがログインしていなければTOPにリダイレクト
-  private def authorize
-    unless user_signed_in?
-      redirect_to controller: :top, action: :index
-    end
-  end
+  # private def authorize
+  #   unless user_signed_in?
+  #     redirect_to controller: :top, action: :index
+  #   end
+  # end
 
   private def book_params
     params.require(:book).permit(:title, :body, :image)
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,17 @@
 class Book < ApplicationRecord
     belongs_to :user
     mount_uploader :image, ImageUploader
+
+    validates :title, presence: true
+    validates :body, presence: true
+    validate :image_not_upload
+
+    def image_not_upload
+        if image.blank?
+            errors.add(:image, 'をアップロードしてください')
+        end
+    end
+
+
+
 end
